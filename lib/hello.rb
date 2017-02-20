2729592f9a0e4d66722d79bb8d64d2c2c4c9e5b3
John Hillegass <cyrodil@Dragonborn.local>
2017-02-20 10:30:53 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,14 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  ar2 = []
+  while i < array.length
+    if array[i].start_with?('T', 't')
+      yield array[i]
+      ar2.push(array[i])
+    end
+    i += 1
+  end
+  ar2
 end
 
 # call your method here!
