c28969c20055f46f2edcc88f67c572eab0fe677f
Jen Poole <contact.vandepool@gmail.com>
2017-08-13 23:32:41 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
-def hello_t
+def hello_t(array)
+  i = 0 # set variable counter
 
+  while i < array.length # execute loop as long as i is less than length of array
+    i = i + 1 # increment i on each iteration
+  end
 end
 
 # call your method here!
-
