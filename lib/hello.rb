c61c4da2b8062ba9812c7a32a11bb93a153c357b
Jen Poole <contact.vandepool@gmail.com>
2017-08-13 23:40:34 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,15 @@
 def hello_t(array)
-  i = 0 # set variable counter
+  if block_given? # if there is a block, execute while loop
+    i = 0 # set variable counter
 
-  while i < array.length # execute loop as long as i is less than length of array
-    yield array[i] # yield each value stored in the array
-    i = i + 1 # increment i on each iteration
+    while i < array.length # execute loop as long as i is less than length of array
+      yield array[i] # yield each value stored in the array
+      i = i + 1 # increment i on each iteration
+    end
+    array # return the original array
+  else # else, output that there isn't a block
+    puts "Hey! No block was given!"
   end
-
-  array # return the original array
 end
 
 # call your method here!
